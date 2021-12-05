4255f43a270cf4325b4979d4442b8ae3705628f4
AndersonPJ <andersonhenrique.ahbs@gmail.com>
2021-12-05 02:27:25 -0300

    add Cal

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	PROTOCOL = "tcp"
+	IP       = "127.0.0.1"
+	PORT     = "12345"
+)
+
+func main() {
+	fmt.Println("Iniciando Servidor...")
+
+	listen, err := net.Listen(PROTOCOL, ":"+PORT)
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	//s := Calculator.Server{}
+	servergRPC := grpc.NewServer()
+
+	if err := servergRPC.Serve(listen); err != nil {
+		log.Fatalf("Failed to Serve: %s", err)
+	}
+}
